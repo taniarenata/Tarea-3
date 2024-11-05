77a014db77ce61fab745eee5402653833937eb22
taniarenata <tania.pillcod@ucuenca.edu.ec>
2024-11-05 16:35:42 -0500

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #Archivo de salida
-printf "%-10s %-20s %-15s %-15s\n" "Tiempo" "% Total de CPU Libre" "% Memoria Libre" "% Disco Libre"
+archivo="monitoreo.txt"
+printf "%-10s %-20s %-15s %-15s\n" "Tiempo" "% Total de CPU Libre" "% Memoria Libre" "% Disco Libre"> $archivo
 
 #Captura de datos cada 60 segundos durante 5 minutos (5 iteraciones)
 for i in {1..5}; do
@@ -11,13 +12,15 @@ for i in {1..5}; do
 	cpulibre=$(top -b -n1 | grep "Cpu(s)" | awk '{print 100 - $2}'| awk -F. '{print $1}')
 	#Porcentaje de memoria libre
 	memlibre=$(free | grep Mem | awk '{print $4/$2 * 100.0}' | awk -F. '{print $1}')
-	#Porcentaje de disco duro libre (usar la particion raiz /)
+	#Porcentaje de disco libre (usar la particion raiz /)
 	discolibre=$(df -h / | grep '/' | awk '{print $5}' | tr -d '%')
+	#Mostrar los resultados en la terminal
+	echo "Tiempo: $tiempo | %Totak de CPU libre: $cpulibre | %Memoria Libre: $memlibre |%Disco libre: $discolibre" 
 	#Imprimir los resultados en formato de tabla
-	printf "%-10s %-20s %-15s %15s\n" "$tiempo" "$cpulibre" "$memlibre" "$discolibre"
+	printf "%-10s %-20s %-15s %15s\n" "$tiempo" "$cpulibre" "$memlibre" "$discolibre">> $archivo
 	#Esperar 60 segundos antes de la proxima iteracion
 	sleep 60
 done
 
-echo "Monitoreo completado."
+echo "Monitoreo completado.Lso resultados se guardaron en $archivo"
 
